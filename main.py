c8b693bd10c83783627e87a59a06e57eb0e7a7cf
Jing <liujingyun0209@gmail.com>
2017-01-21 18:35:07 -0500

    cheryl test

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import csv
+import setup
+import sys
 
 if __name__=="main":
 
 	########################### Prompt the user to calibrate: stand ##################
 	data_stand = []
+	data_tmp = []
+	count = 0
+	while(count < 10000):   # While standing
+		data_tmp = sys.stdin
+		if(data_tmp[1] == ' /muse/acc'):
+			data_stand.append(data_tmp[4])
 	#data_tmp = input from Muse
-	if(data_tmp[1] == ' /muse/acc'):
-		data_stand.append(data_tmp[4])
+	print data_stand
 	bias = sum(data_stand)/len(data_stand)
 	##################### Prompt the user to calibrate: move head right/left ##################
 	data_cal = []
